f6630b533d2a96da0b73dab87a906692a6cab61a
Polina <sunnyfur9@gmail.com>
2023-03-20 01:19:01 +0500

    feat: change styles in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import CardList from './components/cards/CardList';
 import Header from './components/header/Header';
 import { Routes, Route } from 'react-router-dom';
