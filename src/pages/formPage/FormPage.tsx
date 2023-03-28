772e54dbd48a6b12292d1adb84f2e5e0d16c4a52
sunnyfur <polin.makarova@mail.ru>
2023-03-28 17:50:40 +0500

    feat: rewrite form on form hooks

--- src/pages/formPage/FormPage.tsx
+++ src/pages/formPage/FormPage.tsx
@@ -1,57 +1,37 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import CardList from '../../components/cards/CardList';
 import Form from '../../components/form/Form';
 import Message from '../../components/message/Message';
 import { CardType } from '../../types/types';
 import styles from './formPage.module.scss';
-interface Props {
-  children?: React.ReactNode;
-}
-interface State {
-  cards: CardType[];
-  isShowMessage: boolean;
-}
 
-class FormPage extends React.Component<Props, State> {
-  private formRef: React.RefObject<HTMLFormElement>;
-  constructor(props: Props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.formRef = React.createRef<HTMLFormElement>();
-    this.state = {
-      cards: [],
-      isShowMessage: false,
-    };
-  }
-  handleSubmit(card: CardType) {
-    this.setState((prevState: State) => ({
-      cards: [...prevState.cards, card],
-      isShowMessage: true,
-    }));
-    this.formRef.current!.reset();
+const FormPage = () => {
+  const [cards, setCards] = useState<CardType[]>([]);
+  const [isShowMessage, setIsShowMessage] = useState(false);
 
+  const handleSubmit = (card: CardType) => {
+    setCards((prevState) => [...prevState, card]);
+    setIsShowMessage(true);
     setTimeout(() => {
-      this.setState({ isShowMessage: false });
+      setIsShowMessage(false);
     }, 3000);
-  }
-  handleClick() {
-    this.setState({ isShowMessage: false });
-  }
-  render() {
-    return (
-      <section className={styles.wrapper}>
-        <Form refForm={this.formRef} onSubmit={this.handleSubmit} />
-        <CardList cards={this.state.cards} />
+  };
+  const handleClick = useCallback(() => {
+    setIsShowMessage(false);
+  }, []);
 
-        {this.state.isShowMessage && (
-          <Message onClick={this.handleClick}>
-            <p>The data is added</p>
-          </Message>
-        )}
-      </section>
-    );
-  }
-}
+  return (
+    <section className={styles.wrapper}>
+      <Form onSubmit={handleSubmit} />
+      <CardList cards={cards} />
+
+      {isShowMessage && (
+        <Message onClick={handleClick}>
+          <p>The data is added</p>
+        </Message>
+      )}
+    </section>
+  );
+};
 
 export default FormPage;
